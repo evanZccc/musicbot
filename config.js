07bac07d54f6622e70ee4f3189cd6353389863fc
evanZccc <167677714+evanZccc@users.noreply.github.com>
2024-11-11 17:52:04 +0100

    config.js

--- config.js
+++ config.js
@@ -1,35 +1,29 @@
 module.exports = {
-  TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
-  setupFilePath: './commands/setup.json',
-  commandsDir: './commands',  
-  embedColor: "#1db954",
-  musicardTheme:"themes16", //Goes from themes1 to themes19
-  SupportServer: "https://discord.gg/43VDd3UTrM",
-  CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
-  MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
-  embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
-  errorLog: "", 
-  execute(client) {
+    TOKEN: "",
+    ownerID: ["1004206704994566164", ""], 
+    setupFilePath: './commands/setup.json',
+    commandsDir: './commands',  
+    embedColor: "#1db954",
+    musicardTheme:"themes16", //Goes from themes1 to themes19
+    activityName: "OST Inazuma", // This is bot status Write Anything here 
+    activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+    SupportServer: "https://discord.gg/xQF9f9yUEM",
+    CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
+    MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
+    embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
+    errorLog: "", 
   
-        const activities = [
-            { name: 'Inazuma Eleven Italia', type: ActivityType.Watching },
-            { name: 'Inazuma Eleven Victory Road', type: ActivityType.Playing },
-            { name: 'Inazuma Eleven Cheats', type: ActivityType.Watching },
-            { name: 'Inazuma Ost', type: ActivityType.Listening },
-        ];
-}
-   // Lavalink Server Details
-
-  nodes: [
-    {
-        name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
-        reconnectTimeout: 5000,
-        reconnectTries: Infinity,
-        secure: false
-    },
- ]
-}
+     // Lavalink Server Details
+  
+    nodes: [
+      {
+          name: "Server",
+          host: "nodev4.fypmoon.org",
+          port: 1118,
+          password: "fypmoon.org",
+          reconnectTimeout: 5000,
+          reconnectTries: Infinity,
+          secure: false
+      },
+   ]
+  }
