d7b895cf1bd1541d6e9815ef35f566bbe836c11b
evanZccc <167677714+evanZccc@users.noreply.github.com>
2024-11-11 17:42:03 +0100

    config.js

--- config.js
+++ config.js
@@ -21,30 +21,6 @@ module.exports = {
             { name: 'Inazuma Ost', type: ActivityType.Listening },
         ];
 
-     
-        const statuses = ['online', 'idle', 'dnd'];
-
-     
-        let currentActivityIndex = 0;
-        let currentStatusIndex = 0;
-
- 
-        function setActivityAndStatus() {
-        
-            const activity = activities[currentActivityIndex];
-            const status = statuses[currentStatusIndex];
-
-          
-            client.user.setPresence({
-                activities: [activity],
-                status: status,
-            });
-
-            
-            currentActivityIndex = (currentActivityIndex + 1) % activities.length;
-            currentStatusIndex = (currentStatusIndex + 1) % statuses.length;
-        }
-
         
         setTimeout(() => {
             setActivityAndStatus();
