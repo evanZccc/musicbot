53be2c361a0ff01ea4ef1a54a930f9f107e10451
evanZccc <167677714+evanZccc@users.noreply.github.com>
2024-12-10 15:03:52 +0100

    config.js

--- config.js
+++ config.js
@@ -17,11 +17,13 @@ module.exports = {
   
     nodes: [
       {
-  identifier: "INZEWORLD.COM (DE)",
-  password: "saher.inzeworld.com",
-  host: "lava.inzeworld.com",
-  port: 3128,
+  identifier: "Catfein ID",
+  password: "catfein",
+  host: "lava.catfein.com",
+  port: 4000,
   secure: false
 },
    ]
   }
+
+
